67fb190013e05efeaaea4abe9036a72e630498b7
samuel-rapeti26 <samuel.rapeti26@gmail.com>
2023-03-10 16:09:22 +0530

    Grid Toolbar

--- src/components/summary.js
+++ src/components/summary.js
@@ -1,11 +1,46 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import { DataGrid, GridToolbarContainer, GridToolbarExport,useGridApiContext } from "@mui/x-data-grid";
+import { DataGrid, GridToolbarContainer,GridToolbar, GridToolbarExport,useGridApiContext } from "@mui/x-data-grid";
+import { PDFDownloadLink, Document, Page, Text,View,StyleSheet  } from '@react-pdf/renderer';
 import { Button } from "@mui/material";
 import { FileCopy, Print, FileDownload } from "@mui/icons-material";
 import axios from "axios";
 import Cookies from "js-cookie";
+import ItemsTable from "./ItemsTable";
 //ParagraphNum
+const data = {
+  id: "5df3180a09ea16dc4b95f910",
+  items: [
+    {
+      sr: 1,
+      desc: "desc1",
+      xyz: 5,
+    },
+    {
+      sr: 2,
+      desc: "desc2",
+      xyz: 6,
+    },
+  ],};
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+
+const MyDoc = ({data=[]}) => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+    <ItemsTable data={data} />
+    </Page>
+  </Document>
+);
 
 const columns = [
   {
@@ -85,32 +120,43 @@ const Summary = ({ revert ,rowsData,selectedNaratvies,setSelectedNarratives,hand
       console.error(e);
     }
   };
-  function CustomToolbar() {
-    const apiRef = useGridApiContext();
-    const exportDataAsPrint = () => apiRef.current.exportDataAsPrint();
-    //  const exportDataAsExcel = () => apiRef.current.exportDataAsExcel();
-    // const exportDataAsPrint = () => apiRef.current.exportDataAsPrint();
-    // const exportDataAsPrint = () => apiRef.current.exportDataAsPrint();
-    return (
-      <GridToolbarContainer>
-        <div className="flex justify-start items-center mb-3 gap-2">
-        <Button variant="outlined" onClick={() => {}}>
-          <FileCopy />
-        </Button>
-        <Button variant="outlined" onClick={exportDataAsPrint}>
-          <Print />
-        </Button>
-        {/* <Button variant="outlined" onClick={exportDataAsExcel}>
-          <FileDownload /> <span className="ms-1"> Download as Excel </span>
-        </Button>
-        <Button variant="outlined" onClick={() => {}}>
-          <FileDownload /> <span className="ms-1"> Download as PDF </span>
-        </Button> */}
-      </div>
-      </GridToolbarContainer>
-    );
-  }
   let filteredData= rowsData.filter(data=> data.error !== "\n");
+
+  // ######### CUSTOM TOOLBAR
+  // function CustomToolbar() {
+  //   const apiRef = useGridApiContext();
+  //   const exportDataAsPrint = () => apiRef.current.exportDataAsPrint();
+  //   //  const exportDataAsExcel = () => apiRef.current.exportDataAsExcel();
+  //   // const exportDataAsPrint = () => apiRef.current.exportDataAsPrint();
+  //   // const exportDataAsPrint = () => apiRef.current.exportDataAsPrint();
+  //   return (
+  //     <GridToolbarContainer>
+  //       <div className="flex justify-start items-center mb-3 gap-2">
+  //       <Button variant="outlined" onClick={() => {}}>
+  //         <FileCopy />
+  //       </Button>
+  //       <Button variant="outlined" onClick={exportDataAsPrint}>
+  //         <Print />
+  //       </Button>
+  //       {/* <Button variant="outlined" onClick={exportDataAsExcel}>
+  //         <FileDownload /> <span className="ms-1"> Download as Excel </span>
+  //       </Button> */}
+  //         <>
+  //           <PDFDownloadLink document={<MyDoc data={filteredData} />} fileName="somename.pdf">
+  //             {({ blob, url, loading, error }) =>
+  //               loading ? 'Loading document...' : <Button variant="outlined" onClick={() => { }}>
+  //                 <FileDownload /> <span className="ms-1"> Download as PDF </span>
+  //               </Button>
+  //             }
+  //           </PDFDownloadLink>
+  //         </>
+        
+  //     </div>
+  //     </GridToolbarContainer>
+  //   );
+  // }
+  
+  console.log("filteredData98",filteredData);
   return (
     <Box sx={{ width: "100%" }}>
       <p className="mb-3">
@@ -127,7 +173,7 @@ const Summary = ({ revert ,rowsData,selectedNaratvies,setSelectedNarratives,hand
         checkboxSelection
         disableSelectionOnClick
         experimentalFeatures={{ newEditingApi: true }}
-        components={{ Toolbar: CustomToolbar }}
+        components={{ Toolbar: GridToolbar }}
         onSelectionModelChange={setSelectedNarratives}
       />{" "}
       <div className="flex justify-end items-center mt-4 gap-2">
